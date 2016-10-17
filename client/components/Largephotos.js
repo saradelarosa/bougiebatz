0993a6dc9a422c58e623b8da1439f30fc4700281
JakeSmith1 <jakesmith5757@gmail.com>
2016-10-17 17:50:33 -0400

    feat Large photo functional component added for rendering photos, axios get request added, chore debugging

--- client/components/Largephotos.js
+++ client/components/Largephotos.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
+import axios from 'axios';
 
 class Large extends React.Component {
   static contextTypes = {
@@ -10,14 +11,72 @@ class Large extends React.Component {
     super(props)
 
     this.state = {
-
+      photos: [],
+      //default search parameters
+      searchParams: {
+        source: 'all',
+        section: 'all',
+        time: '24',
+        limit: 4
+      }
     }
   }
-    render() {
-      return (
-        <div>HELLO THERE</div>
-      )
 
-    }
+  getPhotos(source, section, time, limit) {
+    //bypassing the server here
+    axios
+    // .get('http://api.nytimes.com/svc/news/v3/content/nyt/business/72.json?limit=15')
+//get params and organize them, add them to req.body
+    .get('api/Large', {
+        params: {
+          source: source || 'all',
+          section: section || 'all',
+          time: time || '24',
+          limit: limit || 4
+        }
+    })
+    .then( (response) => {
+      var multimediaPhotos = response.data.results
+        .filter((photo) => photo.multimedia.length === 4 )
+      console.log(multimediaPhotos, "LargePhoto client response.body");
+      //there is a problem because some articles multimedia is ''
+      this.setState({
+        photos: multimediaPhotos
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  componentDidMount() {
+    this.getPhotos();
+  }
+
+  render() {
+    return (
+      <div>
+      <div>HELLO THERE TEST</div>
+      <LargePhotos
+        photos={this.state.photos}
+        handleSearchParamChange={this.getPhotos.bind(this)}
+      />
+      </div>
+    )
+
+  }
 }
+
+var LargePhotos = ({ photos, handleSearchParamChange }) => (
+  <div className="Large Photos">
+   {photos.map((photo) =>
+     <div> {photo.abstract}
+       <a href={photo.url}>
+         <img src={photo.multimedia[2].url} />
+       </a>
+    </div>
+   )}
+ </div>
+)
+
 export default Large;
