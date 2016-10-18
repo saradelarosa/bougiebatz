5a0957f1f878fd90ae71e24b1d09a38c2e5b014f
Nav <navkiran.ghuman@gmail.com>
2016-10-18 19:51:53 -0400

    bootstrap testing in medium photos

--- client/components/MediumPhotoEntry.js
+++ client/components/MediumPhotoEntry.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import {Grid, Row, Col, rounded} from 'react-bootstrap';
 
 var MediumPhotoEntry = ({mediumPhotos}) => (
   <div className="Medium Photos">
     {mediumPhotos.map((photo) =>
-      <a href={photo.url}>
-        <img className="grow" src={photo.multimedia[1].url} />
-      </a>
+      <Grid>
+        <Row>
+          <Col xs={6} md={4}>
+          <a href={photo.url}>
+            <img className="grow" src={photo.multimedia[1].url}/>
+          </a>
+          </Col>
+        </Row>
+      </Grid>
     )}
   </div>
 );
 
+
+
+// {/* <Grid>
+//    <Row>
+//      <Col xs={6} md={4}>
+//        <Image src="/assets/thumbnail.png" rounded />
+//      </Col>
+//      <Col xs={6} md={4}>
+//        <Image src="/assets/thumbnail.png" circle />
+//      </Col>
+//      <Col xs={6} md={4}>
+//        <Image src="/assets/thumbnail.png" thumbnail />
+//      </Col>
+//    </Row>
+//  </Grid> */}
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 // MediumPhotoEntry.propTypes = {
