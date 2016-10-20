51fa4dbb7bfde440e2f63d556fb43d8f7a7d4cd0
JakeSmith1 <jakesmith5757@gmail.com>
2016-10-20 12:42:34 -0400

    refactor imports removed from homepage

--- client/components/home.js
+++ client/components/home.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
-import axios from 'axios';
-import Select from 'react-select';
-import { Grid, Row, Col } from 'react-bootstrap';
 
 class Home extends React.Component {
   static contextTypes = {
@@ -12,12 +9,11 @@ class Home extends React.Component {
   constructor(props) {
     super(props)
   }
-render() {
-  return (
-    <div>HOME SCREEN</div>
-  )
-}
-
+  render() {
+    return (
+      <div>HOME SCREEN</div>
+    )
+  }
 }
 
 export default Home
