beebb7c4cbc9a925d2c1f97a4b9f53f37bfa152a
JakeSmith1 <jakesmith5757@gmail.com>
2016-10-20 18:08:37 -0400

    bugfix, made small photo entry fade in smoother

--- client/components/Photoentry.js
+++ client/components/Photoentry.js
@@ -4,17 +4,17 @@ import { default as Fade } from 'react-fade';
 //creating template f
 
 var PhotoEntry = ({photos, handleSearchParamChange}) => (
-	<Fade duration={.8}>
 	<div className="smallPhotos">
 	{photos.map((photo, i) =>
+		<Fade duration={.8}>
      <div key={i}>
        <a href={photo.url}>
          <img className="smallPhotoItem" src={photo.multimedia[2].url} />
        </a>
     </div>
+		</Fade>
    )}
  </div>
- </Fade>
 )
 
 
