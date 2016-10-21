0c4cd1027a44b6db6e4f37f33a6a67cc3a97b6a2
JakeSmith1 <jakesmith5757@gmail.com>
2016-10-21 10:46:09 -0400

    refactor server mapping response from db to be the image url

--- server/routes/articleRoutes.js
+++ server/routes/articleRoutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var Article = require('../models/articleModel')
+var Article = require('../models/articleModel');
 
 router.post('/article', function(req, res){
   console.log("++++++++++ RECIEVED REQUEST POST to /article ++++++++++++++");
@@ -19,8 +19,9 @@ router.get('/article', function(req, res){
   console.log("++++++++++ RECIEVED GET TO/article +++++++++++++");
   //get all links from the db
   Article.find({}, function(err, urls) {
+    //urls is an array of objects with properties of articleImageUrl
     res.send(urls.map(function(url) {
-      return url;
+      return url.articleImageUrl;
     }));
   });
 });
