8563270463cbaf1101245dae9ffb6687337b46f0
Sara Sailors <sarasailors@gmail.com>
2016-10-25 14:16:56 -0400

    fix(mlabs): updated

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var dbUrl = 'mongodb://heroku_rm47z500:2p9b9kceqhn15vif8r44h78lho@ds031607.mlab.com:31607/heroku_rm47z500';
+var dbUrl = 'mongodb://heroku_nkhcxv83:d6do54be96ubqk19u6f2vj4jne@ds031925.mlab.com:31925/heroku_nkhcxv83';
 
 //connect to remote mongodb database
 var db = mongoose.connect(dbUrl, function(err, res){
