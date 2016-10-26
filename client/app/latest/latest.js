23296f653b9e91ba37dd4d2d84150405e00d8bfe
ktbarnes <katosych@gmail.com>
2016-10-26 11:28:31 -0400

    Working on logic for latest page, added factory

--- client/app/latest/latest.js
+++ client/app/latest/latest.js
@@ -1,1 +1,32 @@
 angular.module('legacyOwls.latest', [])
+.controller('latest', ['$scope', 'Articles', function($scope, Articles) {
+
+  $scope.options = Articles.options;
+  $scope.selectedOption = 'all';
+
+  $scope.getLatest = function() {
+    var params = {
+      source: 'all',
+      section: $scope.selectedOption,
+      time: '24',
+      limit: 40,
+      offset: 0
+    }
+
+    Articles.getLatest(params)
+    .then(function(response) {
+      $scope.photos = response.data.results.filter(function(photo) {
+        photo.multimedia.length === 4;
+      });
+      console.log($scope.photos);
+    });
+
+  }
+
+  // Get the latest news items
+  $scope.getLatest();
+
+  // Look for newest news every 5 minutes
+  setInterval($scope.getLatest, 5*60000); 
+
+}]);
--- client/factory_functions/factory.js
+++ client/factory_functions/factory.js
@@ -1,5 +1,51 @@
 angular.module('legacyOwls.factory', {})
+.factory('Articles', ['$http', function($http) {
 
-// .factory('', function(){
-//   //your code here
-// })
+  var options = [
+    'all',
+    'arts',
+    'blogs',
+    'books',
+    'briefing',
+    'business',
+    'food',
+    'health',
+    'magazine',
+    'movies',
+    'multimedia',
+    'multimedia/photos',
+    'n.y. / region',
+    'obituaries',
+    'opinion',
+    'public editor',
+    'real estate',
+    'science',
+    'sports',
+    'style',
+    'sunday review',
+    'technology',
+    'theater',
+    'today\'s paper',
+    'travel',
+    'u.s.',
+    'washington',
+    'week in review',
+    'well',
+    'world',
+    'your money'
+  ];
+
+  var getLatest = function(params) {
+    return $http({
+      method: 'GET',
+      url: '/api/Large',
+      params: params
+    })
+  }
+
+  return {
+    options: options,
+    getLatest: getLatest
+  }
+
+}])
