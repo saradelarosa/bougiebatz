df0093350fa2faa24d180d2f187f338fb9a80930
canhtrinh <canh.coding@gmail.com>
2016-10-26 10:40:39 -0400

    new client folder with framework for angular routes and associated dependencies

---
+++ client/app/app.js
@@ -0,0 +1,49 @@
+angular.module('legacyOwls', [
+  //list other dependencies here
+  'legacyOwls.auth',
+  'legacyOwls.home',
+  'legacyOwls.factory',
+  'ngRoute'
+])
+
+.config(function ($routeProvider) {
+  $routeProvider
+      .when('/', {
+          templateUrl: 'app/home/home.html',
+          controller: 'home',
+      })
+      // .when('/latest', {
+      //     templateUrl: 'app/latest/latest.html',
+      //     controller: 'latest',
+      //     access: {restricted: true}
+      // })
+      .when('/login', {
+          templateUrl: 'app/auth/login.html',
+          controller: 'loginController'
+      })
+      .when('/logout', {
+          controller: 'logoutController',
+          access: {restricted: true}
+      })
+      .when('/register', {
+          templateUrl: 'app/auth/register.html',
+          controller: 'registerController'
+      })
+      .otherwise({
+          redirectTo: '/'
+      });
+
+})
+
+.run(function ($rootScope, $location, $route, AuthService) {
+    $rootScope.$on('$routeChangeStart',
+        function (event, next, current) {
+            AuthService.getUserStatus()
+                .then(function () {
+                    if (next.access.restricted && !AuthService.isLoggedIn()) {
+                        $location.path('/login');
+                        $route.reload();
+                    }
+                });
+        });
+});
