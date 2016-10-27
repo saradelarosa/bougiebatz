c1b767245f3745f25d58b0168a585e9a109148cd
ktbarnes <katosych@gmail.com>
2016-10-27 19:58:14 -0400

    Picture and abstract and likes and stuff appears in modal

--- client/app/modal/modalController.js
+++ client/app/modal/modalController.js
@@ -1,6 +1,43 @@
 angular.module('legacyOwls.modal', [])
 
-.controller('modalController', ['$scope', 'close', function ($scope, close) {
+.controller('modalController', ['$scope', 'close', 'Photo', 'Trending', 'SavedArticles', function ($scope, close, Photo, Trending, SavedArticles) {
+
+    $scope.photo = Photo.getPhoto();
+
+    $scope.like = function() {
+
+        Trending.like($scope.photo)
+        .then(function(response) {
+          console.log("Success");
+          $scope.photo.likes++;
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+
+        SavedArticles.saveLikeToDB($scope.photo)
+        .then(function(response) {
+          console.log("Success");
+          $scope.photo.liked = true;
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+
+    }
+
+    $scope.saveStory = function() {
+
+        SavedArticles.saveArticleToDB($scope.photo)
+        .then(function(response) {
+          console.log("Success");
+          $scope.photo.saved = true;
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+
+    }
 
     $scope.close = function (result) {
         close(result, 500); // close, but give 500ms for bootstrap to animate
@@ -8,11 +45,20 @@ angular.module('legacyOwls.modal', [])
 
 }])
 
-.controller('modalMainController', ['$scope', 'ModalService', function ($scope, ModalService) {
+.controller('modalMainController', ['$scope', 'ModalService', 'Photo', function ($scope, ModalService, Photo) {
+
+    // console.log($scope.photos);
 
     $scope.yesNoResult = null;
 
-    $scope.showYesNo = function () {
+    $scope.showYesNo = function (index) {
+
+        var photo = $scope.photos[index];
+
+        photo.saved = $scope.saved[photo.url] ? true : false;
+        photo.liked = $scope.likes[photo.url] ? true: false;
+
+        Photo.savePhoto(photo);
 
         ModalService.showModal({
             templateUrl: "app/modal/modal.html",
