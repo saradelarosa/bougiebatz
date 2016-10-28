9bf79bc4f2b6dd785549a095f80279b345b71e15
canhtrinh <canh.coding@gmail.com>
2016-10-28 17:50:57 -0400

    (fix): amended trending.js to filter out zero likes in the database so they won't be rendered

--- client/app/trending/trending.js
+++ client/app/trending/trending.js
@@ -1,21 +1,25 @@
+//Module that controls trending.html page
 angular.module('legacyOwls.trending', [])
 
 .controller('trendingController', 
   ['$scope', 'Trending',
   function($scope, Trending){
-    $scope.trendingArticles = [];
+    //represents data to be rendered
+    //this will be all data from the "liked" database
+    //that will be sorted then filtered after downloadTrendingArticles is called
+    $scope.trendingArticles = []; 
 
     $scope.downloadTrendingArticles = function(){
 
+      //download all "liked" articles from the "liked" database
       Trending.getAll()
       .then(function(response) {
-        $scope.trendingArticles = response.data;
-        $scope.trendingArticles.sort( function(a, b) { //Have to sort by number of likes decreasing
+        $scope.trendingArticles = response.data
+        .sort( function(a, b) { //Have to sort by number of likes decreasing
           a = a.numberLikes;
           b = b.numberLikes;
           return b-a;
-        }).filter(function(article){
-          console.log();
+        }).filter(function(article){ //Filters out articles w/zero likes
           return (article.numberLikes > 0);
         });
       });
