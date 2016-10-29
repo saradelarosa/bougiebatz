b8eb62c664083d6f9a2642ee3284d98b70202593
ktbarnes <katosych@gmail.com>
2016-10-29 11:00:41 -0400

    Added comments on what functions associate with the modal do

--- client/app/modal/modalController.js
+++ client/app/modal/modalController.js
@@ -1,11 +1,11 @@
 angular.module('legacyOwls.modal', [])
 
 .controller('modalController', ['$scope', 'close', 'Photo', 'Trending', 'SavedArticles', function ($scope, close, Photo, Trending, SavedArticles) {
-
+    // Retrieve information from shared factory about the photo/article to display in the modal
     $scope.photo = Photo.getPhoto();
-
+    // Saves like to user likes and increments likes in trending when Like button is clicked
     $scope.like = function() {
-
+        // Increments likes in trending
         Trending.like($scope.photo)
         .then(function(response) {
           $scope.photo.likes++;
@@ -13,7 +13,7 @@ angular.module('legacyOwls.modal', [])
         .catch(function(err) {
           console.error(err);
         });
-
+        // Saves like to user information so a user cannot like something multiple times
         SavedArticles.saveLikeToDB($scope.photo)
         .then(function(response) {
           $scope.photo.liked = true;
@@ -23,7 +23,7 @@ angular.module('legacyOwls.modal', [])
         });
 
     }
-
+    // Saves story for the user so the user can view it later
     $scope.saveStory = function() {
 
         SavedArticles.saveArticleToDB($scope.photo)
@@ -36,7 +36,7 @@ angular.module('legacyOwls.modal', [])
         });
 
     }
-
+    // Closes the modal
     $scope.close = function (result) {
         close(result, 500); // close, but give 500ms for bootstrap to animate
     };
@@ -44,26 +44,29 @@ angular.module('legacyOwls.modal', [])
 }])
 
 .controller('modalMainController', ['$scope', 'ModalService', 'Photo', function ($scope, ModalService, Photo) {
-
+    // When "See More" button is clicked under the photo, modal pops up
     $scope.showYesNo = function (index) {
-
+        // First want to pass the information about the article that is displayed in the modal using a shared factory
         var photo = $scope.photos[index];
-
+        // Did the user already save or like the article?
         photo.saved = $scope.saved[photo.url] ? true : false;
         photo.liked = $scope.likes[photo.url] ? true : false;
 
         Photo.savePhoto(photo);
-
+        // Modal is shown
         ModalService.showModal({
             templateUrl: "app/modal/modal.html",
             controller: "modalController"
-        }).then(function (modal) {
+        })
+        .then(function (modal) {
             modal.element.modal();
-            modal.close.then(function (result) {
-                // console.log('inside modalController',$scope.photo);
+            modal.close
+            .then(function (result) {
+                // Be sure that we keep record that user has liked an article
                 if ($scope.photo.liked && !$scope.likes[photo.url]) {
                     $scope.likes[photo.url] = true; 
                 }
+                // Be sure that we keep record that the user has saved an article
                 if ($scope.photo.saved && !$scope.saved[photo.url]) {
                     $scope.saved[photo.url] = true;
                 }
